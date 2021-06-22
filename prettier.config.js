04566d7b51961ee1c54eeb0f2da861be276d5b1a
Jefferson Yuri Cunha Burk <jefferson.burk@ambevtech.com.br>
2021-06-21 21:21:03 -0300

    Prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   singleQuote: true,
-  trailingComma: "all",
-  arrowParens: "avoid",
-  endOfLine: "lf",
-  printWidth: 120,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+  printWidth: 80,
 };
